ba505afe5156d11f984bbfaa2a838481eeae1f80
Khushi-2005 <104870133+Khushi-2005@users.noreply.github.com>
2024-07-15 02:45:40 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,77 +1,67 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+from distutils.errors import PreprocessError
 import streamlit as st
+import preprocessor, helper
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
-def fetch_stats(selected_user, df):
-    if selected_user != 'Overall':
-        df = df[df['Name'] == selected_user]
-    num_trans = df.shape[0]
-    deb = df[df['Type'] == 'DEBIT']['Amount'].sum()
-    cre = df[df['Type'] == 'CREDIT']['Amount'].sum()
+st.set_page_config(layout='wide')
+st.sidebar.title("Phonepe Transaction Analysis")
+st.title("TOP STATISTICS")
 
-    debit_data = df[df['Type'] == 'DEBIT']
-    top_debit_persons = debit_data.groupby('Name')['Amount'].sum().nlargest(5).reset_index()
 
-    credit_data = df[df['Type'] == 'CREDIT']
-    top_credit_persons = credit_data.groupby('Name')['Amount'].sum().nlargest(5).reset_index()
+file = st.sidebar.file_uploader('Upload CSV', type=['csv'])
 
-    return num_trans, deb, cre, top_debit_persons, top_credit_persons
+if file is not None:
 
-def total_amount_spent_by_month(selected_user, df):
-    if selected_user != 'Overall':
-        df = df[df['Name'] == selected_user]
-    monthly_data = df.groupby('Month')['Amount'].sum()
+    data = pd.read_csv(file)
 
-    plt.figure(figsize=(14, 6))
-    colors = plt.cm.tab20c.colors[:len(monthly_data)]
-    bars = plt.bar(monthly_data.index, monthly_data.values, color=colors, edgecolor='black')
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.xlabel('Month', fontsize=12, fontweight='bold')
-    plt.ylabel('Total Amount', fontsize=12, fontweight='bold')
-    plt.title('Total Amount Spent by Month', fontsize=14, fontweight='bold')
-    plt.xticks(rotation=45)
-    plt.yticks(fontsize=10)
-    for bar in bars:
-        yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width() / 2, yval + 5, round(yval, 2), ha='center', va='bottom')
-    st.pyplot(plt)
+    df = preprocessor.preprocess(data)
+    st.dataframe(df)
+    
+    user_list = df['Name'].unique().tolist()
+    user_list.sort()
+    user_list.insert(0,"Overall")
+    selected_user = st.sidebar.selectbox("Show analysis wrt Name",user_list)
+    
+    
+    
+    if st.sidebar.button("Show Analysis"):
+        num_trans, deb, cre, top_debit_persons, top_credit_persons = helper.fetch_stats(selected_user,df)
+        st.title("Top Statistics")
+        col1,col2,col3 = st.columns(3)
+        
+        with col1:
+            st.header("No. of Transaction")
+            st.subheader(num_trans)
+        with col2:
+            st.header("Total Debit")
+            st.subheader(deb)
+        with col3:
+            st.header("Total Credit")
+            st.subheader(cre)
+        
+        
+        col4,col4 = st.columns(2)      
+        with col1:
+            top_debit_persons.columns = ['Name', 'Debit Amount']
+            st.write("Top 5 Debit:")
+            st.write(top_debit_persons)
+            
+        with col2:
+            top_debit_persons.columns = ['Name', 'Debit Amount']
+            st.write("Top 5 Credit:")
+            st.write(top_credit_persons)
+        
+        
+        st.title("Total amount Spend by Month")
+        helper.total_amount_spent_by_month(selected_user, df)
+        
+        st.title("Percentage of Transaction Types")
+        helper.Percentage_of_Transaction_Types(selected_user, df)
 
-def Percentage_of_Transaction_Types(selected_user, df):
-    if selected_user != 'Overall':
-        df = df[df['Name'] == selected_user]
-    plt.figure(figsize=(16, 2))
-    transaction_types = df['Type'].value_counts()
-    colors = ['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3']
-    plt.pie(transaction_types, labels=transaction_types.index, autopct='%1.1f%%', startangle=10,
-            colors=colors, shadow=True)
-    plt.title('Percentage of Transaction Types', fontweight='bold', fontsize=20)
-    st.pyplot(plt)
-
-def Dailyamountspend(selected_user, df):
-    if selected_user != 'Overall':
-        df = df[df['Name'] == selected_user]
-    daily_data = df.groupby('Date')['Amount'].sum()
-    plt.figure(figsize=(14, 6))
-    plt.plot(daily_data.index, daily_data.values, marker='o', linestyle='-', color='b')
-    plt.xlabel('Date', fontsize=12, fontweight='bold')
-    plt.ylabel('Total Amount', fontsize=12, fontweight='bold')
-    plt.title('Daily Total Amount Spent', fontsize=14, fontweight='bold')
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.xticks(rotation=45, fontsize=10)
-    plt.yticks(fontsize=10)
-    st.pyplot(plt)
-
-def Dayofweek(selected_user, df):
-    if selected_user != 'Overall':
-        df = df[df['Name'] == selected_user]
-    day_of_week_data = df.groupby('Day_of_Week')['Amount'].sum()
-
-    plt.figure(figsize=(14, 6))
-    plt.bar(day_of_week_data.index, day_of_week_data.values, color='skyblue', edgecolor='black')
-    plt.xlabel('Day of Week', fontsize=12, fontweight='bold')
-    plt.ylabel('Total Amount', fontsize=12, fontweight='bold')
-    plt.title('Total Amount Spent by Day of Week', fontsize=14, fontweight='bold')
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.xticks(rotation=45, ha='right', fontsize=10)
-    plt.yticks(fontsize=10)
-    st.pyplot(plt)
+        st.title("Daily amount spend")
+        helper.Dailyamountspend(selected_user, df)
+        
+        st.title("Day of week")
+        helper.Dayofweek(selected_user, df)
